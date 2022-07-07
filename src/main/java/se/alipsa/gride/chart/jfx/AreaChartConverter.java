801e8767bbf7ac072e05c548e5da356779071280
pernyf <per.nyfelt@nordnet.se>
2022-07-07 16:57:36 +0200

    WIP: basic generic table plotting

---
+++ src/main/java/se/alipsa/gride/chart/jfx/AreaChartConverter.java
@@ -0,0 +1,11 @@
+package se.alipsa.gride.chart.jfx;
+
+import javafx.scene.chart.AreaChart;
+
+public class AreaChartConverter {
+
+
+  public static AreaChart convert(se.alipsa.gride.chart.AreaChart chart) {
+    return null;
+  }
+}
