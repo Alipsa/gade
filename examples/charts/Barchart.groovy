c434de8bbe9ce3b2a0f4d88e9fb0f0d4570923ca
pernyf <per.nyfelt@nordnet.se>
2024-01-11 16:53:01 +0100

    char example updates

--- examples/charts/Barchart.groovy
+++ examples/charts/Barchart.groovy
@@ -4,8 +4,7 @@ import se.alipsa.groovy.charts.*
 
 import static se.alipsa.groovy.matrix.ListConverter.*
 
-
-empData = Matrix.create(
+empData = new Matrix(
     emp_id: 1..5,
     emp_name: ["Rick","Dan","Michelle","Ryan","Gary"],
     salary: [623.3,515.2,611.0,729.0,843.25],
@@ -15,20 +14,10 @@ empData = Matrix.create(
 
 chart = BarChart.createVertical("Salaries", empData, "emp_name", ChartType.BASIC, "salary")
 io.display(chart, "charts barchart")
-file = io.projectFile("barchart.png")
 
+file = io.projectFile("barchart.png")
 jfxChart = Plot.jfx(chart)
-copy = se.alipsa.gade.utils.DeepCopier.deepCopy(jfxChart)
-io.display(jfxChart, true, "jfx barchart") //Values are not set properly when cloning!
+io.display(file)
 jfxFile = io.projectFile("jfxBarchart.png")
 io.save(jfxChart, jfxFile, 640, 480) 
 io.display(jfxFile)
-io.display(jfxChart, true, "jfx barchart2")
-
-Plot.png(chart, file, 800, 600)
-io.display(file)
-file2 = io.projectFile("barchart2.png")
-io.save(chart, file2)
-io.display(file2)
-
-""
--- examples/charts/piechart.groovy
+++ examples/charts/piechart.groovy
@@ -5,7 +5,7 @@ import se.alipsa.groovy.charts.*
 import static se.alipsa.groovy.matrix.ListConverter.*
 
 
-empData = Matrix.create(
+empData = new Matrix(
     emp_id: 1..5,
     emp_name: ["Rick","Dan","Michelle","Ryan","Gary"],
     salary: [623.3,515.2,611.0,729.0,843.25],
@@ -14,7 +14,19 @@ empData = Matrix.create(
 )
 
 chart = PieChart.create("Salaries", empData, "emp_name", "salary")
+chart.style.plotBackgroundColor = new java.awt.Color(30, 30, 128)
+chart.style.chartBackgroundColor = new java.awt.Color(60, 100, 170)
+chart.style.legendBackgroundColor = new java.awt.Color(80, 120, 200)
+chart.style.legendVisible = true
+chart.style.legendPosition = 'RIGHT'
+chart.style.titleVisible = true
+
+// show jfx and swing plotting
 io.display(chart, "jfx piechart")
+swingChart = se.alipsa.groovy.charts.SwingPlot.swing(chart)
+io.display(swingChart, 'swingchart')
+
+// save to file
 file = io.projectFile("piechart.png")
 Plot.png(chart, file, 542, 345)
 io.display(file)
