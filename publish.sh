5b72385e145268dcda0271efb1201d149721c195
pernyf <per.nyfelt@nordnet.se>
2025-02-25 10:47:17 +0100

    improve publish Script

--- publish.sh
+++ publish.sh
@@ -2,9 +2,16 @@
 set -e
 echo "Publish to local repo"
 ./gradlew clean publishToMavenLocal
-mvn -f gade-runner.xml install
-echo "Publishing Gade to Sonatype Maven Central"
-./gradlew release
-echo "Publishing Gade Runner to Sonatype Maven Central"
-mvn -Prelease -f gade-runner.xml deploy
+mvn -f gade-runner.pom install
+if [[ -f ~/.gradle/gradle.properties ]]; then
+  source ~/.gradle/gradle.properties
+  if [[ -n "$sonatypeUsername" ]]; then
+    echo "Publishing Gade to Sonatype Maven Central"
+    ./gradlew release
+    echo "Publishing Gade Runner to Sonatype Maven Central"
+    mvn -Prelease -f gade-runner.pom deploy
+  fi
+else
+  echo "Skipped publish to sonatype"
+fi
 echo "Done!"
