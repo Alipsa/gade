7a4d22a7c89e1a07d219dbad7b8d47231927dac9
Per Nyfelt <per.nyfelt@nordnet.se>
2025-11-16 16:40:00 +0100

    Fix ScriptClassLoaderManagerTest import

--- src/test/java/se/alipsa/gade/ScriptClassLoaderManagerTest.java
+++ src/test/java/se/alipsa/gade/ScriptClassLoaderManagerTest.java
@@ -1,6 +1,7 @@
 package se.alipsa.gade;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
